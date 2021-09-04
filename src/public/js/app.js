6bc2c01de76f56b264238a4a5ba2e80c5eb6337e
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-09-04 15:42:23 +0800

    WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got message : ", message, " from server");
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server");
+})
+
+setTimeout(() => {
+    socket.send("Hello from Browser!");
+}, 10000);
