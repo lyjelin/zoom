0770a9dc8f643dc1a2ed1113d438628a054f679e
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-09-13 22:58:11 +0900

    Admint Panel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import express from "express";
-import SocketIO from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -11,7 +12,16 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+});
+
+instrument(wsServer, {
+    auth: false,
+});
 
 function publicRooms(){
     const {
